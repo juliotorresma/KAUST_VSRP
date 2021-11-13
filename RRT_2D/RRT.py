3c6df3b3c1f51ac9021c328502caf52c1f77aa2a
Julio Torres <61338002+juliotorresma@users.noreply.github.com>
2021-11-13 00:16:33 -0600

    Add files via upload

---
+++ RRT_2D/RRT.py
@@ -0,0 +1,44 @@
+import pygame
+from RRTmain import RRTGraph
+from RRTmain import RRTMAp
+import time
+def main():
+    dimensions = (600,1000)
+    start = (50,50)
+    goal = (510,510)
+    obsdim = 30
+    obsnum = 150
+    iteration = 0
+
+    pygame.init()
+    map = RRTMAp(start,goal,dimensions,obsdim,obsnum)
+    graph = RRTGraph(start,goal,dimensions,obsdim,obsnum)
+
+    obstacles = graph.makeobs()
+
+    map.drawMap(obstacles)
+
+    while(not graph.path_to_goal()):
+        if iteration%15 == 0:
+            X, Y, Parent = graph.bias(goal)
+            pygame.draw.circle(map.map,map.gray,(X[-1],Y[-1]),map.nodeRad+2,0)
+            pygame.draw.line(map.map,map.Blue,(X[-1],Y[-1]),(X[Parent[-1]],Y[Parent[-1]]),
+                                map.edgeThickness)
+        else:
+            X, Y, Parent = graph.expand()
+            pygame.draw.circle(map.map,map.gray,(X[-1],Y[-1]),map.nodeRad+2,0)
+            pygame.draw.line(map.map,map.Blue,(X[-1],Y[-1]),(X[Parent[-1]],Y[Parent[-1]]),
+                                map.edgeThickness)
+        if iteration%5 == 0:
+            pygame.display.update()
+        #time.sleep(0.005)
+        iteration+=1
+    map.drawPath(graph.getPathCoords())
+    pygame.display.update()
+    pygame.event.clear() 
+    pygame.event.wait(0)
+
+    
+
+if __name__=='__main__':
+    main()
