2b35c922be7bdf449bd397ff3efbc012dad558e7
Julio Torres <61338002+juliotorresma@users.noreply.github.com>
2021-11-17 07:52:02 -0600

    Updating Inverse Kinematics Function
    Some changes need to be done for the collision detection, but the program seems to be working pretty well...

--- RRT_3D/RRT.py
+++ RRT_3D/RRT.py
@@ -1,6 +1,7 @@
 import pygame
 import pybullet as p
 import pybullet_data
+from pygame.constants import SCALED
 from RRTmain import RRTGraph
 from RRTmain import RRTMAp
 import time
@@ -10,13 +11,13 @@ import keyboard
 
 
 def main():
-    dimensions = (40,40,60)
-    start = (-15, -15, 10)
-    goal = (-15, 15, 10)
+    dimensions = (3,3,3)
+    start = [-.3, -.5, .5]
+    goal = [.6, .5, .5]
     obsdim = 30
     iteration = 0
     obstacles = []
-    obstacle = [[15.1,0, 15.9],[-15.1, 2.1, 10.9],[10.1, 20.1, 7.9],[10.1, -20.1, 7.9]]
+    obstacle = [[.5,.5,1.3]]
 
     #Initialize world
 
@@ -26,97 +27,88 @@ def main():
         # p.connect(p.SHARED_MEMORY_GUI)
     p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
     # p.resetDebugVisualizerCamera(0.9, -100, -30, [0, 0, 0.4])
-    p.resetDebugVisualizerCamera(80.0, -500, -400, [0, 0, 1.0])
+    p.resetDebugVisualizerCamera(2.0, -5, -4, [.5, 1, .5])
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    planeId = p.loadURDF("plane.urdf")
 
-    model_path = '/assets/robots/kuka/iiwa14_robotiq85.urdf'
+    model_path = '../CS221/rethink/sawyer_description/urdf/sawyer_with_gripper.urdf'
     obstacle_path = '/assets/objects/pybullet_data/cube.urdf'
-    start_end_path = '/assets/objects/pybullet_data/sphere2.urdf'
-    scale = 300
-    scale_obs = 100
-    kukaId = p.loadURDF(model_path, [0, 0, 0], globalScaling=scale*0.1)
-    startId = p.loadURDF(start_end_path, [-15, -15, 10])
-    endId = p.loadURDF(start_end_path, [-15, 15, 10])
-    #p.removeCollisionShape(kukaId)
+
+    kukaId = p.loadURDF(model_path, useFixedBase=1)
+    jointLimits = {'lower':[],'upper':[]}
+    for i in range(14):
+        info = p.getJointInfo(kukaId,i)
+        jointLimits['lower'].append(info[8])
+        jointLimits['upper'].append(info[9])
+    #Draw initial and end point
+    p.addUserDebugText(".",goal,[1,0,0],5)
+    p.addUserDebugText(".",start,[0,1,0],5)
+
     if obstacle:
         for i in obstacle:
-            obs_i = p.loadURDF(obstacle_path, i, useFixedBase=1, flags=2, globalScaling=scale_obs*0.1)
-            #p.removeCollisionShape(obs_i)
+            obs_i = p.loadURDF(obstacle_path, i, useFixedBase=1, flags=2)
             obstacles.append(obs_i)
 
-    p.loadURDF("random_urdfs/001/001.urdf", [0, 0.3, 0.3])
-    p.resetBasePositionAndOrientation(kukaId, [0, 0, 0], [0, 0, 0, 1])
-    pos = [-15, -15, 10]
-        # end effector points down, not up (in case useOrientation==1)
-    orn = p.getQuaternionFromEuler([0, -math.pi, 0])
+    orn = p.getQuaternionFromEuler([0, -math.pi/4, 0])
     jointIndex = [1, 2, 3, 4, 5, 6, 7]
-    kukaEndEffectorIndex = 8
-    numJoints = len(jointIndex)
-    # lower limits for null space
-    ll = [-.967, -2, -2.96, 0.19, -2.96, -2.09, -3.05]
-    # upper limits for null space
-    ul = [.967, 2, 2.96, 2.29, 2.96, 2.09, 3.05]
-    # joint ranges for null space
-    jr = [5.8, 4, 5.8, 4, 5.8, 4, 6]
-    # restposes for null space
-    rp = [0, 0, 0, 0.5 * math.pi, 0, -math.pi * 0.5 * 0.66, 0]
-    # joint damping coefficents
-    jd = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-
-    robot_constrains = [ll,ul,jr,rp,jd]
+    kukaEndEffectorIndex = 9
+   
 
     p.setGravity(0, 0, 0)
 
+    jointPoses = p.calculateInverseKinematics(kukaId,kukaEndEffectorIndex,goal, orn,
+        lowerLimits=jointLimits['lower'], upperLimits=jointLimits['upper'])
+    for _ in range(len(jointPoses)):
+        p.resetJointState(kukaId, _, jointPoses[_])
+    
+    
     #Solve RRT Problem
-    #map = RRTMAp(start,goal,dimensions,obstacles,kukaId,
-    #                   kukaEndEffectorIndex,robot_constrains)
+    
     graph = RRTGraph(start,goal,dimensions,obstacles,kukaId,
-                       kukaEndEffectorIndex,robot_constrains)
+                       kukaEndEffectorIndex,jointLimits['lower'],jointLimits['upper'])
     #Execute RRT simulation
     iteration  = 0 
     
     while(not graph.path_to_goal()):
         n = graph.number_of_nodes()
-        if iteration%5 == 0:
+        if iteration%2 == 0:
             X, Y, Z, Parent = graph.bias(goal)
-            p.addUserDebugLine([X[-1],Y[-1],Z[-1]], [X[Parent[-1]],Y[Parent[-1]],Z[Parent[-1]]],lineColorRGB=[0, 0, 1],lineWidth=3.0)
-            p.addUserDebugText(".",[X[-1],Y[-1],Z[-1]],[0,0,1],5)
+            p.addUserDebugLine([X[-1],Y[-1],Z[-1]], [X[Parent[-1]],Y[Parent[-1]],Z[Parent[-1]]],lineColorRGB=[0, 0, 1],lineWidth=1.0)
+            p.addUserDebugText(".",[X[-1],Y[-1],Z[-1]],[0,0,1],1)
+            print([X[-1],Y[-1],Z[-1]])
         else:
             X, Y, Z, Parent = graph.expand()
-            p.addUserDebugLine([X[-1],Y[-1],Z[-1]], [X[Parent[-1]],Y[Parent[-1]],Z[Parent[-1]]],lineColorRGB=[0, 0, 1],lineWidth=3.0)
-            p.addUserDebugText(".",[X[-1],Y[-1],Z[-1]],[0,0,1],5)
-        #print("Entra")
+            p.addUserDebugLine([X[-1],Y[-1],Z[-1]], [X[Parent[-1]],Y[Parent[-1]],Z[Parent[-1]]],lineColorRGB=[0, 0, 1],lineWidth=1.0)
+            p.addUserDebugText(".",[X[-1],Y[-1],Z[-1]],[0,0,1],1)
         iteration+=1
-        p.stepSimulation()
+        #p.stepSimulation()
     
     print("Coordenadas",graph.getPathCoords())
     path_2_follow = graph.getPathCoords().copy()
     for coords in path_2_follow:
         p.addUserDebugText(".",[coords[0],coords[1],coords[2]],[1,0,0],5)
     i=0
-    '''
+    #Aqui
+    jointPoses = p.calculateInverseKinematics(kukaId,9,start, orn,
+        lowerLimits=jointLimits['lower'], upperLimits=jointLimits['upper'])
+    for _ in range(len(jointPoses)):
+        p.resetJointState(kukaId, _, jointPoses[_])
     while len(path_2_follow) > 0:
         i += 1
-        p.stepSimulation()
         pos = path_2_follow.pop(0)
-        graph.setIK([pos[0],pos[1],pos[2]])
-
+        jointPoses = p.calculateInverseKinematics(kukaId,9,pos, 
+            lowerLimits=jointLimits['lower'], upperLimits=jointLimits['upper'])
+        for _ in range(len(jointPoses)):
+            p.resetJointState(kukaId, _, jointPoses[_])
         p.stepSimulation()
-    '''
-    while(True):
-        p.stepSimulation()
+        time.sleep(0.15)
     
-    #p.disconnect()
+    jointPoses = p.calculateInverseKinematics(kukaId,9,start, orn,
+        lowerLimits=jointLimits['lower'], upperLimits=jointLimits['upper'])
+    for _ in range(len(jointPoses)):
+            p.resetJointState(kukaId, _, jointPoses[_])
     
-
-    
-
-    
-        
-    
-
-
-    
+    p.disconnect()
 
 if __name__=='__main__':
     main()
